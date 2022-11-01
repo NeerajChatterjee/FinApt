d8f1f1dcb47962f49a3f89c59927616648b186bf
Neeraj Chatterjee <neeraj.20204123@mnnit.ac.in>
2022-11-01 23:42:12 +0530

    Fixed minor bugs

--- app/src/main/java/com/shrutislegion/finapt/Shopkeeper/ShopSendBillActivity.kt
+++ app/src/main/java/com/shrutislegion/finapt/Shopkeeper/ShopSendBillActivity.kt
@@ -2,6 +2,7 @@ package com.shrutislegion.finapt.Shopkeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shrutislegion.finapt.R
 
 class ShopSendBillActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/shrutislegion/finapt/Shopkeeper/ShopUpdateInventoryActivity.kt
+++ app/src/main/java/com/shrutislegion/finapt/Shopkeeper/ShopUpdateInventoryActivity.kt
@@ -1,13 +1,16 @@
 package com.shrutislegion.finapt.Shopkeeper
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
 import com.shrutislegion.finapt.R
 import com.shrutislegion.finapt.Shopkeeper.InventoryFragments.ShopAddItemsFragment
 import com.shrutislegion.finapt.Shopkeeper.InventoryFragments.ShopCheckItemsFragment
 import com.shrutislegion.finapt.Shopkeeper.InventoryFragments.ShopEditItemsFragment
 import com.shrutislegion.finapt.Shopkeeper.InventoryFragments.ShopRemoveItemsFragment
 import com.shrutislegion.finapt.databinding.ActivityShopUpdateInventoryBinding
+
 
 class ShopUpdateInventoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,23 +22,53 @@ class ShopUpdateInventoryActivity : AppCompatActivity() {
 
         val fragmentManager = supportFragmentManager.beginTransaction()
 
+        val fragmentLayout = FrameLayout(this)
+        // set the layout params to fill the activity
+        fragmentLayout.layoutParams =
+            ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+
+
         binding.checkItemsCardView.setOnClickListener {
-            fragmentManager.replace(R.id.shop_updateInventory_fragment_container, ShopCheckItemsFragment()).commitAllowingStateLoss()
+            // set an id to the layout
+            fragmentLayout.id = R.id.shop_updateInventory_fragment_container // some positive integer
+
+            // set the layout as Activity content
+            setContentView(fragmentLayout)
+            fragmentManager
+                .add(R.id.shop_updateInventory_fragment_container, ShopCheckItemsFragment()).commit()
             supportActionBar!!.title = "Check Items"
         }
 
         binding.addItemsCardView.setOnClickListener {
-            fragmentManager.replace(R.id.shop_updateInventory_fragment_container, ShopAddItemsFragment()).commitAllowingStateLoss()
+            // set an id to the layout
+            fragmentLayout.id = R.id.shop_updateInventory_fragment_container // some positive integer
+
+            // set the layout as Activity content
+            setContentView(fragmentLayout)
+            fragmentManager.add(R.id.shop_updateInventory_fragment_container, ShopAddItemsFragment()).commit()
             supportActionBar!!.title = "Add Items"
         }
 
         binding.editItemsCardView.setOnClickListener {
-            fragmentManager.replace(R.id.shop_updateInventory_fragment_container, ShopEditItemsFragment()).commitAllowingStateLoss()
+            // set an id to the layout
+            fragmentLayout.id = R.id.shop_updateInventory_fragment_container // some positive integer
+
+            // set the layout as Activity content
+            setContentView(fragmentLayout)
+            fragmentManager.add(R.id.shop_updateInventory_fragment_container, ShopEditItemsFragment()).commit()
             supportActionBar!!.title = "Edit Items"
         }
 
         binding.removeItemsCardView.setOnClickListener {
-            fragmentManager.replace(R.id.shop_updateInventory_fragment_container, ShopRemoveItemsFragment()).commitAllowingStateLoss()
+            // set an id to the layout
+            fragmentLayout.id = R.id.shop_updateInventory_fragment_container // some positive integer
+
+            // set the layout as Activity content
+            setContentView(fragmentLayout)
+            fragmentManager.replace(R.id.shop_updateInventory_fragment_container, ShopRemoveItemsFragment()).commit()
             supportActionBar!!.title = "Remove Items"
         }
 
